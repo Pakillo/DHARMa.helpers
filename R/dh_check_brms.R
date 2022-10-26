5405927bd102ab1b643d4d17dfff93040d3b777d
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-26 18:30:53 +0200

    wrap up

--- R/dh_check_brms.R
+++ R/dh_check_brms.R
@@ -1,6 +1,6 @@
 #' Check Bayesian models fitted with brms
 #'
-#' @param model A fitted model [brms::brmsfit-class()]. 
+#' @param model A fitted model [brms::brmsfit-class()].
 #' @param integer Logical (TRUE/FALSE), indicating if response is an integer, as in Poisson and binomial models
 #' @param plot Logical. Plot residual checks? Default is TRUE.
 #' @param nsamples Integer. Number of samples to draw from the posterior.
@@ -11,42 +11,58 @@
 #' @export
 #' @import DHARMa
 #'
+#' @seealso https://frodriguezsanchez.net/post/using-dharma-to-check-bayesian-models-fitted-with-brms/
+#'
 #' @examples
 #' \dontrun{
+#' # Example models taken from brms::brm()
+#'
 #' # Poisson regression for the number of seizures in epileptic patients
 #' fit1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
 #'            data = epilepsy, family = poisson())
-#' simres <- dh_check_brms(fit1, integer = TRUE) 
+#' simres <- dh_check_brms(fit1, integer = TRUE)
 #' plot(simres, form = epilepsy$zAge)
-#' testDispersion(simres)         
+#' testDispersion(simres)
+#'
+#'
+#' # Probit regression using the binomial family
+#' ntrials <- sample(1:10, 100, TRUE)
+#' success <- rbinom(100, size = ntrials, prob = 0.4)
+#' x <- rnorm(100)
+#' data4 <- data.frame(ntrials, success, x)
+#' fit4 <- brm(success | trials(ntrials) ~ x, data = data4,
+#'             family = binomial("probit"))
+#' summary(fit4)
+#' simres <- dh_check_brms(fit4, integer = TRUE)
+#' plot(simres, form = data4$x)
 #' }
 dh_check_brms <- function(model,             # brms model
                        integer = FALSE,   # integer response? (TRUE/FALSE)
                        plot = TRUE,       # make plot?
                        nsamples = 1000,   # posterior samples in posterior_predict
                        ntrys = 5,         # number of trys in truncated distributions. See posterior_predict
-                       ...                # further arguments for DHARMa::plotResiduals 
+                       ...                # further arguments for DHARMa::plotResiduals
 ) {
-  
+
   mdata <- brms::standata(model)
   if (!"Y" %in% names(mdata))
     stop("Cannot extract the required information from this brms model")
-  
+
   dharma.obj <- DHARMa::createDHARMa(
     simulatedResponse = t(brms::posterior_predict(model, ndraws = nsamples, ntrys = ntrys)),
-    observedResponse = mdata$Y, 
+    observedResponse = mdata$Y,
     fittedPredictedResponse = apply(
       t(brms::posterior_epred(model, ndraws = nsamples, re.form = NA)),
       1,
       mean),
     integerResponse = integer)
-  
+
   if (isTRUE(plot)) {
     plot(dharma.obj, ...)
   }
-  
+
   invisible(dharma.obj)
-  
+
 }
 
 
---
+++ README.md
@@ -0,0 +1,67 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# DHARMa.helpers
+
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/Pakillo/DHARMa.helpers/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/Pakillo/DHARMa.helpers/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
+<https://pakillo.github.io/DHARMa.helpers>
+
+DHARMa.helpers is an R package that facilitates checking fitted
+statistical models via the
+[DHARMa](https://cran.r-project.org/web/packages/DHARMa/vignettes/DHARMa.html)
+package. By now, only Bayesian models fitted with
+[brms](https://paul-buerkner.github.io/brms/) are implemented. See [this
+post](https://frodriguezsanchez.net/post/using-dharma-to-check-bayesian-models-fitted-with-brms/)
+for a detailed explanation of the approach.
+
+## Installation
+
+``` r
+# install.packages("remotes")
+remotes::install_github("Pakillo/DHARMa.helpers")
+```
+
+## Example
+
+``` r
+library(brms)
+library(DHARMa.helpers)
+```
+
+### Poisson regression
+
+``` r
+# Example model taken brms::brm()
+# Poisson regression for the number of seizures in epileptic patients
+fit1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
+            data = epilepsy, family = poisson(), refresh = 0)
+#> Compiling Stan program...
+#> Start sampling
+simres <- dh_check_brms(fit1, integer = TRUE)
+```
+
+<img src="man/figures/README-unnamed-chunk-3-1.png" width="100%" />
+
+``` r
+plot(simres, form = epilepsy$zAge)
+```
+
+<img src="man/figures/README-unnamed-chunk-3-2.png" width="100%" />
+
+``` r
+DHARMa::testDispersion(simres)
+```
+
+<img src="man/figures/README-unnamed-chunk-3-3.png" width="100%" />
+
+    #> 
+    #>  DHARMa nonparametric dispersion test via sd of residuals fitted vs.
+    #>  simulated
+    #> 
+    #> data:  simulationOutput
+    #> dispersion = 1.1911, p-value = 0.14
+    #> alternative hypothesis: two.sided
